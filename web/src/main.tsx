88c384ef77a34c5511eeaf067820b9ec859bd6ea
tnorchard <dexter.fn1@gmail.com>
2026-01-08 18:10:43 -0500

    fix: correct App.css import casing for Vercel

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
-import './app.css'
+import './App.css'
 import { ErrorBoundary } from "./components/ErrorBoundary.tsx"
 
 createRoot(document.getElementById('root')!).render(
